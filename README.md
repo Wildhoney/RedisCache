e9f31ff9ba62816151b0a1592c7682e7594d1aef
Adam Timberlake <adam.timberlake@gmail.com>
2013-10-27 11:40:06 +0000

    Initial release

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Installation
 
 It's a prerequisite to have <a href="http://jasdeep.ca/2012/05/installing-redis-on-mac-os-x/" target="_blank">Redis installed</a>. Once installed you can begin the Redis server with `redis-server`.
 
-Initiate the Node.js server with `node example/server.js` and then point your browser to `localhost:1010`.
+Initiate the Node.js server with `node example/server.js` and then point your browser to `localhost:3000`.
--- example/server.js
+++ example/server.js
@@ -1,9 +1,37 @@
 var express = require('express'),
-    app     = express().listen(1010),
-    redis   = require('redis');
+    fs      = require('fs'),
+    cache   = require('../package/redisCache'),
+    app     = express();
+
+app.listen(3000);
+
+// Connect and configure RedisCache!
+cache.connect(6379).configure({
+    expiry: 86400
+});
 
 app.get('/', function(request, response) {
 
+    cache.fetch('words').otherwise(function(deferred) {
 
+        // Read the file because we don't have a cache object currently.
+        fs.readFile('words.json', 'utf8', function(error, models) {
+
+            // Store the data in the Redis cache object.
+            deferred.resolve(JSON.parse(models));
+
+        });
+
+    }).then(function(models) {
+
+        // We have the data so we can output it to the browser!
+        response.send(models);
+
+    }).fail(function() {
+
+        // Invoked when you reject the promise above.
+        response.send([]);
+
+    });
 
 });
